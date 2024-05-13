cabb65d8d8621947dbcd99bf6e1727eb3864e979
javid shabin <javid.main.act@gmail.com>
2024-05-13 22:27:38 +0530

    add count increase degrees and reset fuction

--- script.js
+++ script.js
@@ -2,3 +2,24 @@
 let number = document.getElementById("number");
 let degBtn = document.getElementById("degrees");
 let incBtn = document.getElementById("increas");
+let reserBtn = document.querySelector(".reset");
+
+let count = 0;
+
+incBtn.addEventListener("click", () => {
+    count++;
+    number.innerHTML = count;
+})
+degBtn.addEventListener("click", () => {
+    if (count > 0) {
+        count--
+        number.innerHTML = count;
+    }
+})
+
+reserBtn.addEventListener("click", () => {
+    if (count > 0) {
+        count = 0
+        number.innerHTML = count
+    }
+})
